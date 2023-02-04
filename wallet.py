a1ee3cf74c4ff4d52338b96b3eace0aac01183fc
Moaz Tahir <88526322+ZackFairMoaz@users.noreply.github.com>
2023-02-04 15:32:32 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -2,11 +2,11 @@ class Wallet:
     def __init__(self):
         self._balance = 0
 
-    def get_ammount(self):
+    def getAmmount(self):
         return self._balance
 
-    def set_ammount(self, value):
+    def setAmmount(self, value):
         self._balance = value
         
-    def remove_ammount(self, value):
+    def removeAmmount(self, value):
         self._balance -= value 
