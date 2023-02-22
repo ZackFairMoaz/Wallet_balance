895b53d75bf85afcd387115200158f741c958a0a
Moaz Tahir <saudjanjua2002@gmail.com>
2023-02-22 15:37:56 +0500

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ def test_getAmount():
     obj=Wallet()
     obj.setAmount(20)
     assert obj.getAmount()==20
+
+def test_getAmount():
+    obj=Wallet()
+    obj.setAmount(40)
+    assert obj.getAmount()==40
